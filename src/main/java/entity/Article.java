762be41a0d78c37129f3995bb1f33767968d4677
dlsb <daniel.sieczkowski@gft.com>
2022-07-11 17:05:36 +0200

    Added flowable dependecies and created Entities.

---
+++ src/main/java/entity/Article.java
@@ -0,0 +1,40 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.flowable.task.api.Task;
+
+import java.util.Map;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Article {
+
+    private UUID articleProcessID;
+
+    private UUID taskID;
+
+    private String author;
+
+    private String title;
+
+    private String articleBody;
+
+    private String status;
+
+    public Article (Task task){
+
+        Map<String, Object> articleVariables = task.getProcessVariables();
+        this.articleProcessID = UUID.fromString(task.getProcessInstanceId());
+        this.taskID = UUID.fromString(task.getId());
+        this.author = (String) articleVariables.get("author");
+        this.title = (String) articleVariables.get("tittle");
+        this.articleBody = (String) articleVariables.get("articleBody");
+        this.status = (String) articleVariables.get("status");
+    }
+}
